568a1b7c278683ea31d39ddf8fffb0734e761d9a
Liam Gallagher <802189@sylvaniastudents.org>
2021-12-01 14:12:23 -0500

    Added Server Getters for Pages

--- server.js
+++ server.js
@@ -2,5 +2,20 @@ const express = require('express')
 const app = new express()
 const db = require('better-sqlite3')('database.db')
 
-app.use(express.static('./client/build'))
-app.use()
+app.use(express.static("public"))
+
+app.get("/#homePage", (req, res) => {
+
+})
+
+app.get("/#search", (req, res) => {
+    
+})
+
+app.get("/#addLocation", (req, res) => {
+
+})
+
+app.get("/#help", (req, res) => {
+    
+})
