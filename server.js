0b102d79ad6a72c2f4a53d8bf25cf5be6726e83e
Liam Gallagher <71441726+LiamGallagher03@users.noreply.github.com>
2021-12-10 16:24:17 -0500

    Fixed

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = new express()
 const db = require('better-sqlite3')('database.db')
 
-app.use(express.static('./client/build'))
-app.use()
+app.use(express.static("public"))
+
+
+    
