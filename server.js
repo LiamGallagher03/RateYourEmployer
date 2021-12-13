b6146903b9c3308655210bc47820e4a0807f31bd
Liam Gallagher <71441726+LiamGallagher03@users.noreply.github.com>
2021-12-13 14:26:50 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
-const app = express()
-const db = require()('TOCE.db')
+const app = new express()
+const db = require('better-sqlite3')('database.db')
 
-app.use(express.static("public"))
+app.use(express.static('./client/build'))
 app.use(express.json())
 
-app.listen(3000, () => {
-    console.log("egg")
+app.get('/reviews',(req, res) => {
+    const query = db.prepare("SELECT * FROM reviewList")
+    const reviews = query.all()
+    res.json({
+        reviews
+    })
+})
+
+app.listen(8080,() => {
+    console.log("server started")
 })
