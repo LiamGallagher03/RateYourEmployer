72ec0383d222f6798bd871826506429a365953d9
Liam Gallagher <71441726+LiamGallagher03@users.noreply.github.com>
2022-01-05 13:43:41 -0500

    Halfworking DB

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express')
 const app = new express()
-const db = require('better-sqlite3')('database.db')
+const db = require('better-sqlite3')('SICC.db')
 
-app.use(express.static('./client/build'))
+app.use(express.static("public"))
 app.use(express.json())
 
-app.get('/reviews',(req, res) => {
-    const query = db.prepare("SELECT * FROM reviewList")
-    const reviews = query.all()
+app.get('/events', (req, res) => {
+    const query = db.prepare("SELECT * FROM Events")
+    const events = query.all()
     res.json({
-        reviews
+        events
     })
 })
 
-app.post("/updateReviews",(req, res) => {
-    const {reviewId} = req.body
-    const query = db.insert(reviewId)
-    res.json({
-        reviewId
-    })
-})
 
-app.listen(8080,() => {
-    console.log("server started")
+app.listen(8080, () => {
+    console.log("egg")
 })
