a5d27b8fd042d34607e0890043bab891ad3bb05a
Liam Gallagher <71441726+LiamGallagher03@users.noreply.github.com>
2022-01-05 13:47:23 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const app = new express()
-const db = require('better-sqlite3')('SICC.db')
+const db = require('better-sqlite3')('database.db')
 
-app.use(express.static("public"))
+app.use(express.static('./client/build'))
 app.use(express.json())
 
-app.get('/events', (req, res) => {
-    const query = db.prepare("SELECT * FROM Events")
-    const events = query.all()
+app.get('/reviews',(req, res) => {
+    const query = db.prepare("SELECT * FROM reviewList")
+    const reviews = query.all()
     res.json({
-        events
+        reviews
     })
 })
 
-
-app.listen(8080, () => {
-    console.log("egg")
+app.listen(8080,() => {
+    console.log("server started")
 })
